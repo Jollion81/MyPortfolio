0cfd46140f9c8496629ccaa8ed089a5d236cbcc9
Ivan_Savchuk <vanya.savchuk@gmail.com>
2024-06-20 21:01:36 +0100

    final fix

--- MyPortfolio.js
+++ MyPortfolio.js
@@ -9,22 +9,22 @@ fetch('project.json').then(function (res) {
         console.log(project)
         target.innerHTML += `
             <div class="project">
-            <div class="projectTitleAndImage"> 
-                <div> 
-                <p>${project.name}</p>
+                <div class="projectTitleAndImage"> 
+                    <div> 
+                        <p>${project.name}</p>
+                    </div>
+                    <div class="imageContainer">
+                        <img class="image" src="${project.img}"/>
+                    </div>
                 </div>
-                <div>
-                    <img class="image" src="${project.img}"/>
-                </div>
-            </div>
 
-            <div> 
-                <p>${project.desc}</p>
-            </div>
-            <div>
-            <a href='${project.ref}'>Github<i class="fa-brands fa-github"></i></a>
-                
-            </div> 
+                <div> 
+                    <p>${project.desc}</p>
+                </div>
+
+                <div>
+                <a href='${project.ref}'>Github<i class="fa-brands fa-github"></i></a>
+                </div> 
             </div>
 
         `
